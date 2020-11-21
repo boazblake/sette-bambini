4b6c3f4c02f421201c8ea5a922b6eeb14c053a83
Boaz Blake <BBlake@goempyrean.com>
2020-11-20 18:29:16 -0600

    build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["app.css","8da85df9ec62725a1615df0c0c856ad4"],["app.js","e05dbbb1b5ceae3f738627747ffe6fa7"],["index.html","5d328bbbae61f1976ccf4ab0177f42b8"]];
+var precacheConfig = [["app.css","b00702ff78244269a691bb44d7ce182a"],["app.js","a05152c05349cba91de5695736ebe46a"],["index.html","5d328bbbae61f1976ccf4ab0177f42b8"],["vendor.js","075b7584dd1d6f0b28d025dfe723f99a"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
