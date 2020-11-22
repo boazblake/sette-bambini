69e3c54a1a6588b0ecb42f5d0f1433dc910a45cd
Boaz Blake <BBlake@goempyrean.com>
2020-11-22 11:55:39 -0600

    making img responsive

--- app/Pages/home.js
+++ app/Pages/home.js
@@ -5,8 +5,8 @@ import { animate,SlideInUp } from "Styles/animations"
 const Home = () => {
   return {
     view: ({ attrs: { mdl } }) =>
-      m(".frow", { id: "home-page" },
-        frontImgs.map(img => m('img.col-md-1-3.animate', { oncreate: SlideInUp, style: { 'max-width': '40%' }, src: img.src })))
+      m(".frow-container", { id: "home-page" },
+        frontImgs.map(img => m('img.col-xs-1-3 col-sm-1-3 col-md-1-3', { src: img.src })))
   }
 }
 
