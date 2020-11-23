3e6e76859af46ed278be127b724705675b57d887
Boaz Blake <BBlake@goempyrean.com>
2020-11-22 18:07:14 -0600

    name image cdn

--- app/index.images.js
+++ app/index.images.js
@@ -1,27 +1,27 @@
 export const collectionImgs = [
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection1.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection2.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection3.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection4.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection5.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection6.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection7.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection8.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection9.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/collection10.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection1.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection2.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection3.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection4.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection5.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection6.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection7.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection8.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection9.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/collection10.jpg?scale.option=fill&format=webp',},
 ]
 export const blanketImgs = [
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/blankets1.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/blankets2.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/blankets3.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/blankets4.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/blankets1.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/blankets2.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/blankets3.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/blankets4.jpg?scale.option=fill&format=webp',},
 
 ]
 export const burpImgs = [
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/burp1.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/burp2.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/burp3.jpg?scale.option=fill&format=webp',},
-  {src:'https://ackstrae.sirv.com/Images/sette-bambini/burp4.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/burp1.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/burp2.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/burp3.jpg?scale.option=fill&format=webp',},
+  {src:'https://sette-bambini.sirv.com/Images/burp4.jpg?scale.option=fill&format=webp',},
 
 ]
 
