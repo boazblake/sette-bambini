8f94901c3db54476741ec8b9a524beafb666370f
Boaz Blake <BBlake@goempyrean.com>
2021-10-11 09:54:37 -0500

    fix manifest

--- app/index.js
+++ app/index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV !== "production") {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("./service-worker.js")
+        .register("./sw.js")
         .then((registration) => {
           console.log("⚙️ SW registered: ", registration)
         })
